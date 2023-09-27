9565ebc4bc2193685fc2a0fe88eaec34a23df49a
sizxle <1840956@students.wits.ac.za>
2023-09-27 18:05:18 +0200

    new feel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { useScroll } from './../context/ScrollContext';
 function Footer() {
   const handleSendToWhatsApp = () => {
     const textareaValue = document.getElementById('messageTextarea').value;
-    const phoneNumber = '+27769314241';
+    const phoneNumber = '+27670174107';
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
       textareaValue
     )}`;
@@ -27,7 +27,7 @@ function Footer() {
       <div id='footer' className='max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5'>
         <div className='flex flex-col gap-5'>
           <img className='w-32' src={logo} alt='Logo' />
-          <p className='text-white text-sm tracking-wide'>ShakesRepair.com</p>
+          <p className='text-white text-sm tracking-wide'>fixIT.com</p>
           <div className='flex gap-5 text-lg text-gray-500'>
             <FaInstagram className='hover:text-white duration-300 cursor-pointer' />
             <FaTwitter className='hover:text-white duration-300 cursor-pointer' />
@@ -37,9 +37,9 @@ function Footer() {
         <div className='text-white'>
           <h2 className='text-2xl font-semibold mb-4'>Locate Us</h2>
           <div className='text-base flex flex-col gap-1'>
-            <p>Zeerust, 018</p>
-            <p>Phone: 065 000 0000</p>
-            <p>e-mail: shakesrepair@gmail.com</p>
+            <p>Zeerust , 018</p>
+            <p>Phone: 067 017 4107</p>
+            <p>e-mail: Fixits018@gmail.com</p>
           </div>
         </div>
         <div className='text-white'>
--- src/components/ServicesCards.jsx
+++ src/components/ServicesCards.jsx
@@ -6,32 +6,32 @@ import iphone2 from './../assets/pngegg.png'
 function ServicesCards() {
   return (
     <div className='mx-2 md:mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center'>
-      {/* <div className='w-full'>
+      <div className='w-full'>
         <div className='w-full h-96 cursor-pointer'>
           <img className='h-full object-cover hover:scale-110 duration-500' src={brokenPhone} alt='phonea' />
         </div>
         <div className='w-full p-2'>
           <h2>Iphone Repair</h2>
-          <ul>
+          {/* <ul>
             <li>Battery Replacement</li>
             <li>Screen Replacement</li>
             <li>Charging port Replacement</li>
             <li>Mic Replacement</li>
-          </ul>
+          </ul> */}
         </div>
-      </div> */}
+      </div>
       <div className='w-full'>
         <div className='w-full h-96 cursor-pointer'>
           <img className='h-full object-cover hover:scale-110 duration-500' src={samsung} alt='phonea' />
         </div>
         <div className='w-full p-2'>
-          <h2>Iphone Repair</h2>
-          <ul>
+          <h2>Samsung Repair</h2>
+          {/* <ul>
             <li>Battery Replacement</li>
             <li>Screen Replacement</li>
             <li>Charging port Replacement</li>
             <li>Mic Replacement</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
       <div className='w-full'>
@@ -40,12 +40,12 @@ function ServicesCards() {
         </div>
         <div className='w-full p-2'>
           <h2>Iphone Repair</h2>
-          <ul>
+          {/* <ul>
             <li>Battery Replacement</li>
             <li>Screen Replacement</li>
             <li>Charging port Replacement</li>
             <li>Mic Replacement</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </div>
